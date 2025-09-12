9bf8a86bdce4214eb64c223a786ece4d69732d72
Daniel obinna <dobi44134@gmail.com>
2025-09-12 12:41:21 +0100

    resolved

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 require('./config/database');
 const app = express();
 const schoolRouter = require('./Routers/schoolRouter');
-const Port = 5000;
 
 app.use(express.json());
 app.use(schoolRouter);
